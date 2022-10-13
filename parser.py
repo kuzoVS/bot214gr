f9175a23101a03b21576182c33dfb368db8e0a8b
Saimon <scoot.saimon.scoot@gmail.com>
2022-10-13 16:35:11 +0300

    1
    1

--- parser.py
+++ parser.py
@@ -143,68 +143,66 @@ def zaminka():
     daaaa = 0
     otvet2 = ""
     otvet = ""
+    zameni = ""
     while True:
-
-        if "214" in group:
-            i = 0
-        else:
-            daaaa:int = 0
-            break
-
-        i = i + 1
-        if "214" == group[i].text.strip():
-            gru = group[i].text.strip()
-            i = i + 2
-            grp = group[i].find_all('td')
-            num1 = grp[0].text.strip()
-            naz1 = grp[1].text.strip()
-            deist1 = grp[2].text.strip()
-            newpar1 = grp[3].text.strip()
-            i = i + 2
-            daaaa:int = 1
-            otvet = al[0].text.strip() + "\n" + "⠀⠀⠀" + str(zampr) + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + str(newpr)
-            if naz1 == "":
-                naz1 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-            num1_1 = int(num1) + 1
-            pervii = num1 + "-" + str(num1_1) + "  " + naz1 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist1 + "  " + newpar1
-            otvet2 = pervii + "\n"
-            grp1 = group[i].find_all('td')
-
-            if len(grp1) > 1:
-                num2 = grp1[0].text.strip()
-                naz2 = grp1[1].text.strip()
-                deist2 = grp1[2].text.strip()
-                newpar2 = grp1[3].text.strip()
-                num2_2 = int(num2) + 1
-                if naz2 == "":
-                    naz2 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-                vtoroi = num2 + "-" + str(num2_2) + "  " + naz2 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist2 + "  " + newpar2
-                otvet2 = otvet2 + vtoroi + "\n"
-            else:
+        try:
+            i = i + 1
+            if i > len(group):
+                zameni = "Замен нет"
                 break
-            i = i + 2
-            grp2 = group[i].find_all('td')
-            if len(grp2) > 1:
-                num3 = grp2[0].text.strip()
-                naz3 = grp2[1].text.strip()
-                deist3 = grp2[2].text.strip()
-                newpar3 = grp2[3].text.strip()
-                num3_3 = int(num2) + 1
-                if naz3 == "":
-                    naz3 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-                tretii = num3 + "-" + str(num3_3) + "  " + naz3 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist3 + "  " + newpar3
-                otvet2 = otvet2 + tretii
-            else:
+            if "214" == group[i].text.strip():
+                gru = group[i].text.strip()
+                i = i + 2
+                grp = group[i].find_all('td')
+
+                num1 = grp[0].text.strip()
+                naz1 = grp[1].text.strip()
+                deist1 = grp[2].text.strip()
+                newpar1 = grp[3].text.strip()
+                i = i + 2
+                daaaa:int = 1
+                otvet = al[0].text.strip() + "\n" + "⠀⠀⠀" + str(zampr) + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + str(newpr)
+                if naz1 == "":
+                    naz1 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                num1_1 = int(num1) + 1
+                pervii = num1 + "-" + str(num1_1) + "  " + naz1 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist1 + "          " + newpar1
+                otvet2 = pervii + "\n"
+                grp1 = group[i].find_all('td')
+                zameni = otvet + "\n" + otvet2
+                if len(grp1) > 3:
+                    num2 = grp1[0].text.strip()
+                    naz2 = grp1[1].text.strip()
+                    deist2 = grp1[2].text.strip()
+                    newpar2 = grp1[3].text.strip()
+                    num2_2 = int(num2) + 1
+                    if naz2 == "":
+                        naz2 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                    vtoroi = num2 + "-" + str(num2_2) + "  " + naz2 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist2 + "          " + newpar2
+                    otvet2 = otvet2 + vtoroi + "\n"
+                    zameni = otvet + "\n" + otvet2
+                else:
+                    break
+                i = i + 2
+                grp2 = group[i].find_all('td')
+                if len(grp2) > 1:
+                    num3 = grp2[0].text.strip()
+                    naz3 = grp2[1].text.strip()
+                    deist3 = grp2[2].text.strip()
+                    newpar3 = grp2[3].text.strip()
+                    num3_3 = int(num2) + 1
+                    if naz3 == "":
+                        naz3 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                    tretii = num3 + "-" + str(num3_3) + "  " + naz3 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist3 + "          " + newpar3
+                    otvet2 = otvet2 + tretii
+                    zameni = otvet + "\n" + otvet2
+                    break
+                else:
+                    zameni = otvet + "\n" + otvet2
+                    break
                 break
-            break
+        except:
+            zameni = "Замен нет"
 
-        else:
-            est:int = 0
-
-    if daaaa != 1:
-        zameni = "Замен нет"
-    else:
-        zameni = otvet + "\n" + otvet2
     return zameni
 
 def zvonki():
@@ -219,4 +217,3 @@ def zvonki():
 9. 16:35 - 17:20
 10. 17:25 - 18:10"""
     return zvonok
-
