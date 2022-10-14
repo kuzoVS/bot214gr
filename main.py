dfe5e226e6efecff75c4771f6aed3753a471975d
Saimon <scoot.saimon.scoot@gmail.com>
2022-10-15 00:23:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,28 +16,38 @@ async def bot_messange(messange: types.Message):
 
     if messange.text == 'Замены🔄':
         await bot.send_message(messange.from_user.id, pr.zaminka())
+
     elif messange.text == '📋':
         await bot.send_message(messange.from_user.id, 'Выбери день мучений😩.', reply_markup= nav.otherMenu)
+
     elif messange.text == '⬅':
         await bot.send_message(messange.from_user.id,'⬅', reply_markup= nav.mainMenu)
+
     elif messange.text == '🔔':
         await bot.send_message(messange.from_user.id, pr.zvonki())
+
     elif messange.text == 'Понедельник':
         await bot.send_message(messange.from_user.id, pr.ponedelnik() + "\n" + "Всего две пары, ты уверен, что ты хочешь просто проебать время?🙄.")
+
     elif messange.text == 'Вторник':
         await bot.send_message(messange.from_user.id, pr.vtornik() + "\n" + "Заебись, ко 2 паре, можно и поспать😴.")
+
     elif messange.text == 'Среда':
         await bot.send_message(messange.from_user.id, pr.sreda() + "\n" + "Типо дефолт😐.")
+
     elif messange.text == 'Четверг':
         await bot.send_message(messange.from_user.id, pr.chetverg() + "\n" + "Нахуй ты на четверг смотришь если ДО🙄.")
+
     elif messange.text == 'Пятница':
         await bot.send_message(messange.from_user.id, pr.pyatnica() + "\n" + "Четыре пары блять😭.")
+
     elif messange.text == 'Суббота':
         await bot.send_message(messange.from_user.id, pr.sybbota() + "\n" + "Зачем вообще сегодня куда-то ехать?🥺")
+        await bot.delete_message(messange.from_user.id, messange.message_id)
     else:
         await messange.reply('Ты дурак? Зачем🤬 ты мне пишешь? Просто нажимай на кнопки.', reply_markup= nav.mainMenu)
-        await bot.delete_message(messange.from_user.id, messange.message_id, reply_markup=nav.mainMenu)
-    
+        await bot.delete_message(messange.from_user.id, messange.message_id)
+
    #...........................................Я НЕ ЗНАЮ КАК ВЫВЕСТИ ВОТ ЭТО, ТИПО ЕСЛИ ПОСЛЕ ЭТОГО ('Ты еблан? Нахуй ты мне пишешь? Просто нажимай на кнопки') ПОЛЬЗОВАТЕЛЬ ОТПРАВЛЯЕТ ЕЩЕ СООБЩЕНИЕ, ТО ОН ВЫВЕДЕТ СЛЕДУЮЩЕЕ, НИЖЕ, ПОМОГИ ПОЖАЛУЙСТА!!!!!!!
    #await messange.reply('Повторяю для одаренного, не заебывай меня, я создан только расписания. Если тебе не с кем пообщаться, то это уже не мои проблемы🖕.')
 
