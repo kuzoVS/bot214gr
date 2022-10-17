8a8be9991b4578626b97bd8f22fbedae1aa3e00a
Saimon <scoot.saimon.scoot@gmail.com>
2022-10-17 23:01:10 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -172,31 +172,34 @@ def zaminka():
                 gru = group[i].text.strip()
                 i = i + 2
                 grp = group[i].find_all('td')
-
                 num1 = grp[0].text.strip()
                 naz1 = grp[1].text.strip()
+                if naz1 == "":
+                    naz1 = "Поставили пару"
                 deist1 = grp[2].text.strip()
                 newpar1 = grp[3].text.strip()
+                if newpar1 == "":
+                    newpar1 == "Нет пары"
                 i = i + 2
                 daaaa:int = 1
-                otvet = al[0].text.strip() + "\n" + "⠀⠀⠀" + str(zampr) + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + str(newpr)
-                if naz1 == "":
-                    naz1 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                otvet = al[0].text.strip() + "\n\n" + str(zampr) #+ str(newpr)
                 num1_1 = int(num1) + 1
-                pervii = num1 + "-" + str(num1_1) + "  " + naz1 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist1 + "          " + newpar1
-                otvet2 = pervii + "\n"
+                pervii = num1 + "-" + str(num1_1) + "  " + naz1 + "\n\n" + "Заменяющий препод\n" + str(deist1) + "\n\n" + str(newpr) + "\n" + str(newpar1)
+                otvet2 = pervii + "\n" + "--------------------------------------" + "\n"
                 grp1 = group[i].find_all('td')
                 zameni = otvet + "\n" + otvet2
                 if len(grp1) > 3:
                     num2 = grp1[0].text.strip()
                     naz2 = grp1[1].text.strip()
+                    if naz2 == "":
+                        naz2 = "Поставили пару"
                     deist2 = grp1[2].text.strip()
                     newpar2 = grp1[3].text.strip()
+                    if newpar2 == "":
+                        newpar2 == "Нет пары"
                     num2_2 = int(num2) + 1
-                    if naz2 == "":
-                        naz2 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-                    vtoroi = num2 + "-" + str(num2_2) + "  " + naz2 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist2 + "          " + newpar2
-                    otvet2 = otvet2 + vtoroi + "\n"
+                    vtoroi = num2 + "-" + str(num2_2) + "  " + naz2 + "\n\n" + "Заменяющий препод\n" + str(deist2) + "\n\n" + str(newpr) + "\n" + str(newpar2)
+                    otvet2 = otvet2 + vtoroi + "\n" + "--------------------------------------" + "\n"
                     zameni = otvet + "\n" + otvet2
                 else:
                     break
@@ -205,13 +208,15 @@ def zaminka():
                 if len(grp2) > 1:
                     num3 = grp2[0].text.strip()
                     naz3 = grp2[1].text.strip()
+                    if naz3 == "":
+                        naz3 = "Поставили пару"
                     deist3 = grp2[2].text.strip()
                     newpar3 = grp2[3].text.strip()
+                    if newpar3 == "":
+                        newpar3 == "Нет пары"
                     num3_3 = int(num2) + 1
-                    if naz3 == "":
-                        naz3 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-                    tretii = num3 + "-" + str(num3_3) + "  " + naz3 + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" + deist3 + "          " + newpar3
-                    otvet2 = otvet2 + tretii
+                    tretii = num3 + "-" + str(num3_3) + "  " + naz3 + "\n\n" + "Заменяющий препод\n" + str(deist3) + "\n\n" + str(newpr) + "\n" + str(newpar3)
+                    otvet2 = otvet2 + tretii + "\n" + "--------------------------------------" + "\n"
                     zameni = otvet + "\n" + otvet2
                     break
                 else:
@@ -220,7 +225,6 @@ def zaminka():
                 break
         except:
             zameni = "Замен нет"
-
     return zameni
 
 def zvonki():
